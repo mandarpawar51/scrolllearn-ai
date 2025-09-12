f24785a53b13d0b6f03a18adc05b92f5cca00d2e
mandarpawar51 <mandarpawar18@gmail.com>
2025-09-12 16:29:18 +0530

    removed tests

--- test/basic_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Basic test that should always pass', () {
-    expect(1 + 1, equals(2));
-    print('✅ Basic test passed');
-  });
-}
